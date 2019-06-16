87277773a63bfbee4cf5b9259b34294699dd71f5
nhoa <ainhoa.larumbe@gmail.com>
2019-06-16 21:54:16 +0100

    new home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Recipe } from './Recipe'
+import { Home } from './Home'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
+          <Route path="/" exact component={Home} />
           <Route path="/recipes/:id" component={Recipe} />
         </div>
       </Router>
---
+++ src/Home.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Button, Form, FormGroup, Input, Container, Row, Col  } from 'reactstrap';
+
+export class Home extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { search : '' };
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSearchChange(event) {
+    const value = event.target.value;
+
+    this.setState({
+      search: value
+    });
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.search);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <Form onSubmit={this.handleSubmit}>
+          <Input type="text" name="search" id="searchText" onChange={this.handleSearchChange} value={this.state.search} />
+          <Button>Search</Button>
+        </Form>
+        <SearchResults searchQuery={this.state.search} />
+      </div>
+    )
+  }
+}
