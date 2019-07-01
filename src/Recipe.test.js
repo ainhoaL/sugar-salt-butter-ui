d0b6ffe4763d096f2e475b17af565284b561d823
nhoa <ainhoa.larumbe@gmail.com>
2019-07-01 20:40:45 +0100

    add coverage and move to standard

--- src/Recipe.test.js
+++ src/Recipe.test.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { mount } from 'enzyme';
-import axios from 'axios';
-import { Recipe } from './Recipe';
+import React from 'react'
+import { mount } from 'enzyme'
+import axios from 'axios'
+import { Recipe } from './Recipe'
 
-jest.mock('axios');
+jest.mock('axios')
 
 const recipeData = {
   data: {
@@ -14,52 +13,51 @@ const recipeData = {
     tags: [],
     instructions: ''
   }
-};
+}
 
 describe('Recipe', () => {
-
   beforeEach(() => {
-    axios.get.mockResolvedValue(recipeData);
-  });
+    axios.get.mockResolvedValue(recipeData)
+  })
 
   it('gets recipe by Id on render', () => {
-    const match = { params: { id: 'testId' }};
-    const div = document.createElement('div');
-    const wrapper = mount(<Recipe location={location} match={match} />);
+    const match = { params: { id: 'testId' } }
+    const location = { search: '' }
+    mount(<Recipe location={location} match={match} />)
 
-    expect(axios.get).toHaveBeenCalledTimes(1);
-    expect(axios.get).toHaveBeenCalledWith('http://localhost:3050/api/v1/recipes/testId');
-  });
+    expect(axios.get).toHaveBeenCalledTimes(1)
+    expect(axios.get).toHaveBeenCalledWith('http://localhost:3050/api/v1/recipes/testId')
+  })
 
   it('renders form when edit is set to true', async () => {
-    const match = { params: { id: '1234' }};
-    const location = { search: '?edit=true' };
-    const wrapper = mount(<Recipe location={location} match={match} />);
+    const match = { params: { id: '1234' } }
+    const location = { search: '?edit=true' }
+    const wrapper = mount(<Recipe location={location} match={match} />)
 
-    await axios;
-    wrapper.update(); // Re-render component
-    expect(wrapper.find('ReadonlyRecipe').length).toEqual(0);
-    expect(wrapper.find('EditableRecipe').length).toEqual(1);
-  });
-
-  it('renders readonly recipe when edit is not set', async () => {
-    const match = { params: { id: '1234' }};
-    const location = { search: '?edit=false' };
-    const wrapper = mount(<Recipe location={location} match={match} />);
-
-    await axios;
-    wrapper.update(); // Re-render component
-    expect(wrapper.find('ReadonlyRecipe').length).toEqual(1);
-    expect(wrapper.find('EditableRecipe').length).toEqual(0);
-  });
+    await axios
+    wrapper.update() // Re-render component
+    expect(wrapper.find('ReadonlyRecipe').length).toEqual(0)
+    expect(wrapper.find('EditableRecipe').length).toEqual(1)
+  })
 
   it('renders readonly recipe when edit is not set', async () => {
-    const match = { params: { id: '1234' }};
-    const wrapper = mount(<Recipe location={location} match={match} />);
+    const match = { params: { id: '1234' } }
+    const location = { search: '?edit=false' }
+    const wrapper = mount(<Recipe location={location} match={match} />)
 
-    await axios;
-    wrapper.update(); // Re-render component
-    expect(wrapper.find('ReadonlyRecipe').length).toEqual(1);
-    expect(wrapper.find('EditableRecipe').length).toEqual(0);
-  });
-});
+    await axios
+    wrapper.update() // Re-render component
+    expect(wrapper.find('ReadonlyRecipe').length).toEqual(1)
+    expect(wrapper.find('EditableRecipe').length).toEqual(0)
+  })
+
+  it('renders readonly recipe when edit is not set', async () => {
+    const match = { params: { id: '1234' } }
+    const wrapper = mount(<Recipe location={location} match={match} />)
+
+    await axios
+    wrapper.update() // Re-render component
+    expect(wrapper.find('ReadonlyRecipe').length).toEqual(1)
+    expect(wrapper.find('EditableRecipe').length).toEqual(0)
+  })
+})
