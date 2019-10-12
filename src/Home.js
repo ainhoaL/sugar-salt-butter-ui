e0e13667b3e71ff3ae705a99eada6ef91b9d7e72
nhoa <ainhoa.larumbe@gmail.com>
2019-10-12 12:00:16 +0100

    show search results

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-import { Button, Form, Input } from 'reactstrap'
+import { Button, Form, Input, Card, CardImg, CardBody, CardTitle, CardDeck } from 'reactstrap'
+
+const axios = require('axios')
 
 export class Home extends Component {
   constructor (props) {
     super(props)
-    this.state = { search: '' }
+    this.state = { search: '', searchResults: null }
     this.handleSearchChange = this.handleSearchChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
@@ -18,18 +20,53 @@ export class Home extends Component {
   }
 
   handleSubmit (event) {
-    alert('A name was submitted: ' + this.state.search)
     event.preventDefault()
+
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.props.idToken
+    axios.get('http://localhost:3050/api/v1/recipes/search?searchString=' + this.state.search)
+      .then((response) => { // TODO: deal with error
+        console.log(response.data)
+        this.setState({ searchResults: response.data })
+      })
   }
 
   render () {
     return (
       <div>
-        <Form onSubmit={this.handleSubmit}>
+        <Form inline onSubmit={this.handleSubmit}>
           <Input type='text' name='search' id='searchText' onChange={this.handleSearchChange} value={this.state.search} />
           <Button>Search</Button>
         </Form>
+        {this.state.searchResults ?
+          <div><span>{this.state.searchResults.length} results</span>
+            <ul style={{width:"100%", display:"block"}}>
+              {this.state.searchResults.map((recipe) =>
+                <RecipeCard key={recipe._id} data={recipe} />
+              )}
+            </ul>
+          </div>
+          : null}
       </div>
+    )
+  }
+}
+
+class RecipeCard extends Component {
+  render() {
+    const linkToRecipe = 'recipes/' + this.props.data._id
+    return (
+      // <Card style={{width:"25%"}}>
+      //   <CardImg top width="100%" src={this.props.data.image} />
+      //   <CardBody>
+      //     <CardTitle>{this.props.data.title}</CardTitle>
+      //   </CardBody>
+      // </Card>
+      <li style={{width:"202px", height: "250px", display:"inline-block", "vertical-align":"top", margin:"6px"}}>
+        <a href={linkToRecipe} style={{border:"1px solid #E1E3DF", display:"block", height:"100%", "text-decoration":"none"}}>
+          <img src={this.props.data.image} style={{width:"200px", height:"200px", "object-fit":"cover"}} /><br />
+          <span style={{color:"#222"}}>{this.props.data.title}</span>
+        </a>
+      </li>
     )
   }
 }
