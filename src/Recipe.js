aa7466598a61f522a144df69cfbdaf2961ed7921
ainhoaL <ainhoaL@users.noreply.github.com>
2020-04-03 18:51:41 +0100

    for recipe display use hooks instead of component

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,40 +1,28 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, Form, FormGroup, Label, Input, Container, Row, Col, Badge } from 'reactstrap'
 import qs from 'qs'
 
 const axios = require('axios')
 
-export class Recipe extends Component {
-  constructor (props) {
-    super(props)
-    this.state = { edit: false, recipeId: '', recipe: {} }
-    this.getRecipe = this.getRecipe.bind(this)
-  }
+export function Recipe (props) {
+  const [edit, setEdit] = useState(false)
+  const [recipe, setRecipe] = useState({})
 
-  componentDidMount () {
-    const { params } = this.props.match
-    this.setState({ recipeId: params.id })
+  useEffect(() => {
+    const { params } = props.match
 
-    const { search } = this.props.location
+    const { search } = props.location
     if (search) {
       let queryObj = qs.parse(search.substring(1, search.length))
-      this.setState({
-        edit: queryObj.edit
-      })
+      setEdit(queryObj.edit)
     }
 
-    if (this.props.idToken) {
-      this.getRecipe(this.props.idToken, params.id)
+    if (props.idToken) {
+      getRecipe(props.idToken, params.id)
     }
-  }
+  }, [props])
 
-  componentDidUpdate (prevProps) {
-    if (this.props.idToken && this.props.idToken !== prevProps.idToken) {
-      this.getRecipe(this.props.idToken, this.state.recipeId)
-    }
-  }
-
-  getRecipe (idToken, recipeId) {
+  const getRecipe = (idToken, recipeId) => {
     axios.defaults.headers.common['Authorization'] = 'Bearer ' + idToken
     axios.get('http://localhost:3050/api/v1/recipes/' + recipeId)
       .then((response) => { // TODO: deal with error
@@ -70,36 +58,34 @@ export class Recipe extends Component {
             delete recipe.macros
           }
         }
-        this.setState({
-          recipe: recipe
-        })
+        setRecipe(recipe)
       })
   }
 
-  render () {
-    if (!this.state.recipe || !this.state.recipe.title) {
-      // TODO: loading message?
-      return null
-    }
-    return (
-      <Container>
-        <Row>
-          <Col sm='12' md={{ size: 8, offset: 2 }}>
-            <img src={this.state.recipe.image} alt={this.state.recipe.title} style={{ 'maxWidth': '100%', 'maxHeight': 400 }} />
-            { this.state.edit === 'true'
-              ? <EditableRecipe initialRecipe={this.state.recipe} />
-              : <ReadonlyRecipe recipe={this.state.recipe} />
-            }
-          </Col>
-        </Row>
-      </Container>
-    )
+  if (!recipe || !recipe.title) {
+    // TODO: loading message?
+    return null
   }
+  return (
+    <Container>
+      <Row>
+        <Col sm='12' md={{ size: 8, offset: 2 }}>
+          <img src={recipe.image} alt={recipe.title} style={{ 'maxWidth': '100%', 'maxHeight': 400 }} />
+          { edit === 'true'
+            ? <EditableRecipe initialRecipe={recipe} />
+            : <ReadonlyRecipe recipe={recipe} />
+          }
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
-export class EditableRecipe extends Component {
-  constructor (props) {
-    super(props)
+export function EditableRecipe (props) {
+  const [recipe, setRecipe] = useState({})
+  const [updatedRecipe, setUpdatedRecipe] = useState(false)
+
+  useEffect(() => {
     let recipe = props.initialRecipe
     if (recipe.tags) {
       recipe.tags = recipe.tags.join(', ')
@@ -114,31 +100,25 @@ export class EditableRecipe extends Component {
     }).join('\n')
 
     recipe.ingredientList = ingredientList
+    setRecipe(recipe)
+  }, [props])
 
-    this.state = { recipe: recipe, updatedRecipe: false }
-
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  handleChange (event) {
+  const handleChange = (event) => {
     const target = event.target
     const value = target.type === 'checkbox' ? target.checked : target.value
     const name = target.name
 
-    let newRecipe = { ...this.state.recipe }
+    let newRecipe = { ...recipe }
     newRecipe[name] = value
 
-    this.setState({
-      recipe: newRecipe
-    })
+    setRecipe(newRecipe)
   }
 
-  handleSubmit (event) {
+  const handleSubmit = (event) => {
     event.preventDefault()
 
     // Recreate macros structure in recipe object
-    let recipeObject = { ...this.state.recipe }
+    let recipeObject = { ...recipe }
     recipeObject.macros = { calories: recipeObject.calories, protein: recipeObject.protein, carbs: recipeObject.carbs, fat: recipeObject.fat }
     delete recipeObject.calories
     delete recipeObject.protein
@@ -148,206 +128,202 @@ export class EditableRecipe extends Component {
     recipeObject.ingredients = recipeObject.ingredientList
     delete recipeObject.ingredientList
 
-    axios.put('http://localhost:3050/api/v1/recipes/' + this.state.recipe._id, recipeObject)
+    axios.put('http://localhost:3050/api/v1/recipes/' + recipe._id, recipeObject)
       .then((response) => {
-        this.setState({ updatedRecipe: true })
+        setUpdatedRecipe(true)
       })
   }
 
-  render () {
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        <FormGroup>
-          <Label for='titleText'>Title</Label>
-          <Input type='text' name='title' id='titleText' value={this.state.recipe.title} onChange={this.handleChange} />
-        </FormGroup>
-        <FormGroup>
-          <Label for='urlText'>Link</Label>
-          <Input type='text' name='url' id='urlText' value={this.state.recipe.url} onChange={this.handleChange} />
-        </FormGroup>
-        <FormGroup>
-          <Label for='sourceText'>Source</Label>
-          <Input type='text' name='url' id='sourceText' value={this.state.recipe.source} onChange={this.handleChange} />
-        </FormGroup>
-        <FormGroup>
-          <Label for='authorText'>Author</Label>
-          <Input type='text' name='author' id='authorText' value={this.state.recipe.author} onChange={this.handleChange} />
-        </FormGroup>
-        <FormGroup>
-          <Label for='imageText'>Image Url</Label>
-          <Input type='text' name='image' id='imageText' value={this.state.recipe.image} onChange={this.handleChange} />
-        </FormGroup>
-        <FormGroup>
-          <Label for='tagsText'>Tags</Label>
-          <Input type='text' name='tags' id='tagsText' value={this.state.recipe.tags} onChange={this.handleChange} />
-        </FormGroup>
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <Label for='titleText'>Title</Label>
+        <Input type='text' name='title' id='titleText' value={recipe.title} onChange={handleChange} />
+      </FormGroup>
+      <FormGroup>
+        <Label for='urlText'>Link</Label>
+        <Input type='text' name='url' id='urlText' value={recipe.url} onChange={handleChange} />
+      </FormGroup>
+      <FormGroup>
+        <Label for='sourceText'>Source</Label>
+        <Input type='text' name='url' id='sourceText' value={recipe.source} onChange={handleChange} />
+      </FormGroup>
+      <FormGroup>
+        <Label for='authorText'>Author</Label>
+        <Input type='text' name='author' id='authorText' value={recipe.author} onChange={handleChange} />
+      </FormGroup>
+      <FormGroup>
+        <Label for='imageText'>Image Url</Label>
+        <Input type='text' name='image' id='imageText' value={recipe.image} onChange={handleChange} />
+      </FormGroup>
+      <FormGroup>
+        <Label for='tagsText'>Tags</Label>
+        <Input type='text' name='tags' id='tagsText' value={recipe.tags} onChange={handleChange} />
+      </FormGroup>
+      <Row form>
+        <Col md={4}>
+          <FormGroup>
+            <Label for='servingsText'>Servings</Label>
+            <Input type='text' name='servings' id='servingsText' value={recipe.servings} onChange={handleChange} />
+          </FormGroup>
+        </Col>
+        <Col md={4}>
+          <FormGroup>
+            <Label for='prepTimeText'>Prep Time</Label>
+            <Input type='text' name='prepTime' id='prepTimeText' value={recipe.prepTime} onChange={handleChange} />
+          </FormGroup>
+        </Col>
+        <Col md={4}>
+          <FormGroup>
+            <Label for='cookingTimeText'>Cooking Time</Label>
+            <Input type='text' name='cookingTime' id='cookingTimeText' value={recipe.cookingTime} onChange={handleChange} />
+          </FormGroup>
+        </Col>
+      </Row>
+      <FormGroup>
+        <Label for='ingredientsText'>Ingredients</Label>
+        <Input type='textarea' name='ingredientList' id='ingredientListText' value={recipe.ingredientList} onChange={handleChange} />
+      </FormGroup>
+      <FormGroup>
+        <Label for='instructionsText'>Instructions</Label>
+        <Input type='textarea' name='instructions' id='instructionsText' value={recipe.instructions} onChange={handleChange} />
+      </FormGroup>
+      <FormGroup>
+        <Label for='storageText'>Storage</Label>
+        <Input type='text' name='storage' id='storageText' value={recipe.storage} onChange={handleChange} />
+      </FormGroup>
+      <FormGroup check>
+        <Label check>
+          <Input type='checkbox' name='freezes' id='freezesCheck' checked={recipe.freezes} onChange={handleChange} />{' '}
+            Freezes
+        </Label>
+      </FormGroup>
+      <FormGroup>
+        <Label for='notesText'>Notes</Label>
+        <Input type='textarea' name='notes' id='notesText' value={recipe.notes} onChange={handleChange} />
+      </FormGroup>
+      <FormGroup>
+        <Label for='equipmentText'>Equipment</Label>
+        <Input type='text' name='equipment' id='equipmentText' value={recipe.equipment} onChange={handleChange} />
+      </FormGroup>
+      <div>
         <Row form>
-          <Col md={4}>
+          <Col md={3}>
             <FormGroup>
-              <Label for='servingsText'>Servings</Label>
-              <Input type='text' name='servings' id='servingsText' value={this.state.recipe.servings} onChange={this.handleChange} />
+              <Label for='caloriesText'>Calories</Label>
+              <Input type='text' name='calories' id='caloriesText' value={recipe.calories} onChange={handleChange} />
             </FormGroup>
           </Col>
-          <Col md={4}>
+          <Col md={3}>
             <FormGroup>
-              <Label for='prepTimeText'>Prep Time</Label>
-              <Input type='text' name='prepTime' id='prepTimeText' value={this.state.recipe.prepTime} onChange={this.handleChange} />
+              <Label for='proteinText'>Protein</Label>
+              <Input type='text' name='protein' id='proteinText' value={recipe.protein} onChange={handleChange} />
             </FormGroup>
           </Col>
-          <Col md={4}>
+          <Col md={3}>
             <FormGroup>
-              <Label for='cookingTimeText'>Cooking Time</Label>
-              <Input type='text' name='cookingTime' id='cookingTimeText' value={this.state.recipe.cookingTime} onChange={this.handleChange} />
+              <Label for='carbsText'>Carbs</Label>
+              <Input type='text' name='carbs' id='carbsText' value={recipe.carbs} onChange={handleChange} />
+            </FormGroup>
+          </Col>
+          <Col md={3}>
+            <FormGroup>
+              <Label for='fatText'>Fat</Label>
+              <Input type='text' name='fat' id='fatText' value={recipe.fat} onChange={handleChange} />
             </FormGroup>
           </Col>
         </Row>
-        <FormGroup>
-          <Label for='ingredientsText'>Ingredients</Label>
-          <Input type='textarea' name='ingredientList' id='ingredientListText' value={this.state.recipe.ingredientList} onChange={this.handleChange} />
-        </FormGroup>
-        <FormGroup>
-          <Label for='instructionsText'>Instructions</Label>
-          <Input type='textarea' name='instructions' id='instructionsText' value={this.state.recipe.instructions} onChange={this.handleChange} />
-        </FormGroup>
-        <FormGroup>
-          <Label for='storageText'>Storage</Label>
-          <Input type='text' name='storage' id='storageText' value={this.state.recipe.storage} onChange={this.handleChange} />
-        </FormGroup>
-        <FormGroup check>
-          <Label check>
-            <Input type='checkbox' name='freezes' id='freezesCheck' checked={this.state.recipe.freezes} onChange={this.handleChange} />{' '}
-            Freezes
-          </Label>
-        </FormGroup>
-        <FormGroup>
-          <Label for='notesText'>Notes</Label>
-          <Input type='textarea' name='notes' id='notesText' value={this.state.recipe.notes} onChange={this.handleChange} />
-        </FormGroup>
-        <FormGroup>
-          <Label for='equipmentText'>Equipment</Label>
-          <Input type='text' name='equipment' id='equipmentText' value={this.state.recipe.equipment} onChange={this.handleChange} />
-        </FormGroup>
-        <div>
-          <Row form>
-            <Col md={3}>
-              <FormGroup>
-                <Label for='caloriesText'>Calories</Label>
-                <Input type='text' name='calories' id='caloriesText' value={this.state.recipe.calories} onChange={this.handleChange} />
-              </FormGroup>
-            </Col>
-            <Col md={3}>
-              <FormGroup>
-                <Label for='proteinText'>Protein</Label>
-                <Input type='text' name='protein' id='proteinText' value={this.state.recipe.protein} onChange={this.handleChange} />
-              </FormGroup>
-            </Col>
-            <Col md={3}>
-              <FormGroup>
-                <Label for='carbsText'>Carbs</Label>
-                <Input type='text' name='carbs' id='carbsText' value={this.state.recipe.carbs} onChange={this.handleChange} />
-              </FormGroup>
-            </Col>
-            <Col md={3}>
-              <FormGroup>
-                <Label for='fatText'>Fat</Label>
-                <Input type='text' name='fat' id='fatText' value={this.state.recipe.fat} onChange={this.handleChange} />
-              </FormGroup>
-            </Col>
-          </Row>
-        </div>
-        <FormGroup>
-          <Label for='ratingText'>Rating</Label>
-          <Input type='text' name='rating' id='ratingText' value={this.state.recipe.rating} onChange={this.handleChange} />
-        </FormGroup>
-        <FormGroup check inline>
-          <Label check>
-            <Input type='checkbox' id='tryCheck' name='wantToTry' checked={this.state.recipe.wantToTry} onChange={this.handleChange} />{' '}
+      </div>
+      <FormGroup>
+        <Label for='ratingText'>Rating</Label>
+        <Input type='text' name='rating' id='ratingText' value={recipe.rating} onChange={handleChange} />
+      </FormGroup>
+      <FormGroup check inline>
+        <Label check>
+          <Input type='checkbox' id='tryCheck' name='wantToTry' checked={recipe.wantToTry} onChange={handleChange} />{' '}
             Want to try
-          </Label>
-        </FormGroup>
-        <FormGroup check inline>
-          <Label check>
-            <Input type='checkbox' id='doneCheck' name='done' checked={this.state.recipe.done} onChange={this.handleChange} />{' '}
+        </Label>
+      </FormGroup>
+      <FormGroup check inline>
+        <Label check>
+          <Input type='checkbox' id='doneCheck' name='done' checked={recipe.done} onChange={handleChange} />{' '}
             Tried
-          </Label>
-        </FormGroup>
-        <br /><br />
-        <Button type='submit'>Update</Button>
-        { this.state.updatedRecipe
-          ? <i> Recipe updated</i>
-          : null }
-        <br /><br />
-      </Form>
-    )
-  }
+        </Label>
+      </FormGroup>
+      <br /><br />
+      <Button type='submit'>Update</Button>
+      { updatedRecipe
+        ? <i> Recipe updated</i>
+        : null }
+      <br /><br />
+    </Form>
+  )
 }
 
-export class ReadonlyRecipe extends Component {
-  render () {
-    const recipe = this.props.recipe
-    let listTags
-    if (recipe && recipe.tags) {
-      listTags = recipe.tags.map((tag) =>
-        <Badge color='secondary' key={tag} pill>{tag}</Badge>
-      )
+export function ReadonlyRecipe (props) {
+  const recipe = props.recipe
+  let listTags
+  if (recipe && recipe.tags) {
+    listTags = recipe.tags.map((tag) =>
+      <Badge color='secondary' key={tag} pill>{tag}</Badge>
+    )
+  }
+
+  let ingredientList = recipe.ingredientList.map((item, index) => {
+    if (item.groupHeader) {
+      return <React.Fragment key={index}><strong>{item.groupHeader}: </strong><br /></React.Fragment>
+    } else {
+      return <React.Fragment key={index}>{item.ingredient}<br /></React.Fragment>
     }
+  })
 
-    let ingredientList = recipe.ingredientList.map((item, index) => {
-      if (item.groupHeader) {
-        return <React.Fragment key={index}><strong>{item.groupHeader}: </strong><br /></React.Fragment>
-      } else {
-        return <React.Fragment key={index}>{item.ingredient}<br /></React.Fragment>
-      }
-    })
-
-    return (
-      <div>
-        <h2>{recipe.title}</h2>
-        <a href={recipe.url} target='blank'><h4>{recipe.source} by {recipe.author}</h4></a>
-        <p>
-          { recipe.servings
-            ? <span>Servings: {recipe.servings}</span>
-            : null }
-          { recipe.prepTime
-            ? <span>Prep time: {recipe.prepTime}</span>
-            : null }
-          { recipe.cookingTime
-            ? <span>Cooking time: {recipe.cookingTime}</span>
-            : null }
-        </p>
-        <div style={{ 'whiteSpace': 'pre-line' }}><strong>Ingredients: </strong><br />
-          {ingredientList}
-        </div>
-        <div style={{ 'whiteSpace': 'pre-line' }}><strong>Instructions: </strong><br />
-          {recipe.instructions}
-        </div>
-        { recipe.storage
-          ? <p><strong>Storage: </strong><br />
-            {recipe.storage}
-          </p>
+  return (
+    <div>
+      <h2>{recipe.title}</h2>
+      <a href={recipe.url} target='blank'><h4>{recipe.source} by {recipe.author}</h4></a>
+      <p>
+        { recipe.servings
+          ? <span>Servings: {recipe.servings}</span>
           : null }
-        { recipe.notes
-          ? <p><strong>Notes: </strong><br />
-            {recipe.notes}
-          </p>
-          : null
-        }
-        { recipe.equipment
-          ? <p><strong>Equipment: </strong><br />
-            {recipe.equipment}
-          </p>
-          : null
-        }
-        { recipe.calories
-          ? <p>Calories: {recipe.calories}
+        { recipe.prepTime
+          ? <span>Prep time: {recipe.prepTime}</span>
+          : null }
+        { recipe.cookingTime
+          ? <span>Cooking time: {recipe.cookingTime}</span>
+          : null }
+      </p>
+      <div style={{ 'whiteSpace': 'pre-line' }}><strong>Ingredients: </strong><br />
+        {ingredientList}
+      </div>
+      <div style={{ 'whiteSpace': 'pre-line' }}><strong>Instructions: </strong><br />
+        {recipe.instructions}
+      </div>
+      { recipe.storage
+        ? <p><strong>Storage: </strong><br />
+          {recipe.storage}
+        </p>
+        : null }
+      { recipe.notes
+        ? <p><strong>Notes: </strong><br />
+          {recipe.notes}
+        </p>
+        : null
+      }
+      { recipe.equipment
+        ? <p><strong>Equipment: </strong><br />
+          {recipe.equipment}
+        </p>
+        : null
+      }
+      { recipe.calories
+        ? <p>Calories: {recipe.calories}
             Protein: {recipe.protein}
             Carbs: {recipe.carbs}
             Fat: {recipe.carbs}
-          </p>
-          : null
-        }
-        {listTags}
-      </div>
-    )
-  }
+        </p>
+        : null
+      }
+      {listTags}
+    </div>
+  )
 }
